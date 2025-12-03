bcf344b08ca9aaea68266f67fab7770fcd555f6b
Mina Mahmoodi <min.mahmoodi89@gmail.com>
2025-12-03 20:36:43 +0100

    Add sutble cursor glow effect

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const glow = document.querySelector(".mouse-glow");
+
+document.addEventListener("mousemove", (e) => {
+  glow.style.setProperty("--mouse-x", `${e.clientX}px`);
+  glow.style.setProperty("--mouse-y", `${e.clientY}px`);
+});
