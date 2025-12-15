8821acb4cc354c290883b6970469ef148d15e981
Mina Mahmoodi <min.mahmoodi89@gmail.com>
2025-12-15 16:57:20 +0100

    Show only the first 3projects in the project section

--- scripts/sections/projects.js
+++ scripts/sections/projects.js
@@ -3,39 +3,40 @@ import { projectDetails } from "../../data/projectDetails.js";
 export function renderProjects() {
   let HTML = "";
 
-  projectDetails.forEach((project) => {
-    HTML += `
-      <div class="project-grid">
-        <div class="project-pic">
-          <img
-            src="${project.image.src}"
-            alt="${project.image.alt}"
-          />
-        </div>
-        <div class="project-details">
-          <div>
-            <p class="project-title">
-              <a href="${project.gitHubLink}"
-                >${project.name}</a
-              >
-            </p>
+  projectDetails.forEach((project, index) => {
+    if (index < 3) {
+      HTML += `
+        <div class="project-grid">
+          <div class="project-pic">
+            <img
+              src="${project.image.src}"
+              alt="${project.image.alt}"
+            />
           </div>
-          <div class="description">
-            <p class="project-description">
-              ${project.description}
-            </p>
+          <div class="project-details">
+            <div>
+              <p class="project-title">
+                <a href="${project.gitHubLink}" target="_blank"
+                  >${project.name}</a
+                >
+              </p>
+            </div>
+            <div class="description">
+              <p class="project-description">
+                ${project.description}
+              </p>
+            </div>
+            <div>
+              <ul class="tech-stack">
+                ${project.techStack
+                .map(item => `<li>${item}</li>`)
+                .join("")}
+              </ul>
+            </div>
           </div>
-          <div>
-            <ul class="tech-stack">
-              ${project.techStack
-              .map(item => `<li>${item}</li>`)
-              .join("")}
-            </ul>
-          </div>
-        </div>
-      </div> 
-    `;
+        </div> 
+      `;
+    }    
   });
-
   document.querySelector('.js-project-grid').innerHTML = HTML;
 }
